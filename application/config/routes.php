c1740f80381b0542bc2af4a7d992be8f3b11a161
novilfahlevy <novilfreon@gmail.com>
2019-08-15 16:26:18 +0800

    menghapus searchbox dari navbar

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,10 +49,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'dashboard';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
+// Auth Routes
 $route['login'] = 'auth/login';
 $route['register'] = 'auth/register';
 $route['logout'] = 'auth/logout';
