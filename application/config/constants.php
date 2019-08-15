5d5c5a3a42db291b7d4b19b83b00d70f891b16b4
novilfahlevy <novilfreon@gmail.com>
2019-08-15 13:56:25 +0800

    set konfigurasi ci

--- application/config/constants.php
+++ application/config/constants.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+define('ASSETS', 'http://localhost/myapp/assets');
+
 /*
 |--------------------------------------------------------------------------
 | Display Debug backtrace
