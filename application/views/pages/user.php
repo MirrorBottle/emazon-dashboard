0f35a24649cc0636cd418a0b846da3534717ba1b
novilfahlevy <novilfreon@gmail.com>
2019-08-19 21:06:33 +0800

    membuat halaman user

--- application/views/pages/user.php
+++ application/views/pages/user.php
@@ -2,12 +2,19 @@
 	<div class="col-6 mx-auto mx-md-0 col-md-4 col-lg-3">
 		<img src="<?= ASSETS; ?>img/default.jpg" width="100%" height="200">
 	</div>
-	<div class="col-lg-6">
-		
+	<div class="col-lg-6 p-0 pl-4 pl-lg-0">
+		<h3 class="mt-2">M. Novil Fahlevy</h3>
+		<h5 class="text-muted mt-2">novilfreon@gmail.com</h5>
+		<h5 class="text-muted mt-2 mb-md-5 mb-2">User</h5>
+
+		<button type="button" class="btn btn-primary" id="profile">Profile</button>
+		<button type="button" class="btn btn-secondary" id="edit">Edit</button>
 	</div>
 </div>
 
-<div class="row">
+<div class="w-100"><hr></div>	
+
+<div class="row pl-3">
 	<div class="col-md-6 mb-4 mb-md-0">
 		<h4>About</h4>
 		<div class="row my-2">
@@ -15,7 +22,7 @@
 			<div class="col-3">0895339906176</div>
 		</div>
 		<div class="row my-2">
-			<div clas	s="col-3">Email</div>
+			<div class="col-3">Email</div>
 			<div class="col-3">novilfreon@gmail.com</div>
 		</div>
 		<div class="row my-2">
@@ -38,3 +45,40 @@
 		<p class="my-2 text-muted"><u>Change his name 3 hours ago.</u></p>
 	</div>
 </div>
+
+<!-- <form class="mb-3" method="post" enctype="multipart/form-data">
+	<h3 class="my-4">Edit Profile</h3>
+
+	<div class="form-row">
+		<div class="form-group col">
+		 	<input type="text" class="form-control" id="name" placeholder="Name">
+			<span class="text-small text-danger ml-1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, qui.</span>
+		</div>
+	</div>
+
+	<div class="form-row mb-4">
+		<div class="form-group col-md-6">
+		 	<input type="password" class="form-control" id="password" placeholder="Password">
+			 <span class="text-small text-danger ml-1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, qui.</span>
+		</div>
+		<div class="form-group col-md-6">
+		  	<input type="password" class="form-control" id="confirmPassword" placeholder="Confirm Password">
+			  <span class="text-small text-danger ml-1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, qui.</span>
+		</div>
+	</div>
+
+	<div class="form-group">
+		<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+		<span class="text-small text-danger ml-1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, qui.</span>
+	</div>
+
+	<div class="form-row ml-1">
+		<div class="form-group">
+			<label for="photoProfile">Photo Profile</label>
+			<input type="file" class="form-control-file" id="photoProfile">
+			<span class="text-small text-danger">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, qui.</span>
+		</div>
+	</div>
+
+	<button type="submit" class="btn btn-primary ml-1">Sign in</button>
+</form> -->
