4f9dbb2ac528c37ca511a415f89f96984a916ab5
novilfahlevy <novilfreon@gmail.com>
2019-08-21 14:21:46 +0800

    membuat fitur edit access menu

--- application/views/pages/management/menu.php
+++ application/views/pages/management/menu.php
@@ -19,16 +19,16 @@
 						</td>
 						<td class="align-middle">1</td>
 						<td class="align-middle text-right">
-							<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editMenuModal"
-								tooltip="true" title="Edit" data-placement="top">
+							<button type="button" class="btn btn-primary" data-toggle="modal"
+								data-target="#editMenuModal" tooltip="true" title="Edit" data-placement="top">
 								<span class="fas fa-pencil-alt"></span>
 							</button>
-							<button type="button" class="btn btn-success" data-toggle="modal" data-target="#accessMenuModal"
-								tooltip="true" title="Access" data-placement="top">
+							<button type="button" class="btn btn-success" data-toggle="modal"
+								data-target="#accessMenuModal" tooltip="true" title="Access" data-placement="top">
 								<span class="fas fa-lock"></span>
 							</button>
-							<button type="button" class="btn btn-danger" id="deleteMenu" data-toggle="modal" data-target="#deleteMenuModal"
-								tooltip="true" title="Delete" data-placement="top">
+							<button type="button" class="btn btn-danger" id="deleteMenu" data-toggle="modal"
+								data-target="#deleteMenuModal" tooltip="true" title="Delete" data-placement="top">
 								<span class="fas fa-trash-alt"></span>
 							</button>
 						</td>
@@ -40,7 +40,8 @@
 </div>
 
 <!-- { Modal } Edit Menu -->
-<div class="modal fade" id="editMenuModal" tabindex="-1" role="dialog" aria-labelledby="editMenuModal" aria-hidden="true">
+<div class="modal fade" id="editMenuModal" tabindex="-1" role="dialog" aria-labelledby="editMenuModal"
+	aria-hidden="true">
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
@@ -76,6 +77,55 @@
 	</div>
 </div>
 
+<!-- { Modal } Access Menu -->
+<div class="modal fade" id="accessMenuModal" tabindex="-1" role="dialog" aria-labelledby="accessMenuModal"
+	aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="accessMenuModal">Edit role access to this menu.</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<form method="post">
+				<div class="modal-body">
+					<div class="table-responsive">
+						<table class="table mb-0" width="100%" cellspacing="0">
+							<thead>
+								<tr>
+									<th class="table-bordered">ID</th>
+									<th class="table-bordered border-right-0">Role</th>
+									<th class="table-bordered border-left-0"></th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td class="align-middle table-bordered">1</td>
+									<td class="align-middle table-bordered border-right-0">
+										<label class="form-check-label" for="role1">
+											Admin
+										</label>
+									</td>
+									<td class="text-right table-bordered border-left-0">
+										<div class="form-check">
+											<input class="form-check-input" type="checkbox" value="" id="role1">
+										</div>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-success">Change</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
 <!-- { Modal } Delete Menu -->
 <div class="modal fade" id="deleteMenuModal" tabindex="-1" role="dialog" aria-labelledby="deleteMenuModal"
 	aria-hidden="true">
@@ -88,11 +138,12 @@
 				</button>
 			</div>
 			<div class="modal-body">
-                Are you sure you want to delete 'Dashboard' menu?
+				Are you sure you want to delete 'Dashboard' menu?
 				<div class="alert alert-info p-1 text-center mt-3 mb-1">
 					<span class="text-muted" id="codeDeleteMenu"></span>
 				</div>
-				<input type="text" class="form-control" id="confirmCode" placeholder="Enter code above to confirm deleting menu...">
+				<input type="text" class="form-control" id="confirmCode"
+					placeholder="Enter code above to confirm deleting menu...">
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
